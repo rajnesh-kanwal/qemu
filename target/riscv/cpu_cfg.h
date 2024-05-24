86b787d27f446b484d8210427cea3347fe4886b2
Rajnesh Kanwal <rkanwal@rivosinc.com>
2024-05-24 15:14:58 +0100

    target/riscv: Add support for Control Transfer Records extension CSRs.
    This commit adds support for [m|s|vs]ctrcontrol, sctrstatus and
    sctrdepth CSRs handling.
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

--- target/riscv/cpu_cfg.h
+++ target/riscv/cpu_cfg.h
@@ -131,6 +131,8 @@ struct RISCVCPUConfig {
     bool ext_zvfhmin;
     bool ext_smaia;
     bool ext_ssaia;
+    bool ext_smctr;
+    bool ext_ssctr;
     bool ext_sscofpmf;
     bool ext_smepmp;
     bool ext_smrnmi;
