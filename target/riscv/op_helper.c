4bfefa5cf3321557d0767ab1b4fd102eeaea107c
Rajnesh Kanwal <rkanwal@rivosinc.com>
2024-05-24 15:32:21 +0100

    target/riscv: Add CTR sctrclr instruction.
    CTR extension adds a new instruction sctrclr to quickly
    clear the recorded entries buffer.
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

--- target/riscv/op_helper.c
+++ target/riscv/op_helper.c
@@ -461,6 +461,11 @@ void helper_ctr_branch(CPURISCVState *env, target_ulong src, target_ulong dest,
     }
 }
 
+void helper_ctr_clear(CPURISCVState *env)
+{
+    riscv_ctr_clear(env);
+}
+
 void helper_wfi(CPURISCVState *env)
 {
     CPUState *cs = env_cpu(env);
